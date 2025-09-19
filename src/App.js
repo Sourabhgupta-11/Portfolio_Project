01ca1abfd72674379a6c225d113a01bc89a416d3
Sourabh Gupta <sourabhgupta.1331@gmail.com>
2025-09-19 16:43:55 +0530

    create a basic app with required components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import Navbar from './components/Navbar.js';
+import Hero from './components/Hero.js';
+import About from './components/About.js';
+import Projects from './components/Projects.js';
+import Skills from './components/Skills.js';
+import Contact from './components/Contact.js';
+import Footer from './components/Footer.js';
+import AOS from 'aos';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar/>
+      <main>
+        <Hero />
+        <About />
+        <Projects />
+        <Skills />
+        <Contact />
+      </main>
+      <Footer />
+    </>
   );
 }
 
 export default App;
+
